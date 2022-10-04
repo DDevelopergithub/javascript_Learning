cba39285dd347fb596c1489122840a8801fa5029
DDevelopergithub <tejaswinibhosale787@gmail.com>
2022-10-04 17:26:10 +0530

    debugging  the file
    debugging  the file

---
+++ demo.js
@@ -0,0 +1,12 @@
+   //This is my first program
+  console.log("Hello");
+  console.log("My name is Mohit sharma");
+  //info means information
+  console.info("This is info");
+  //warn means warning
+  console.warn("I am not that bad guy");
+  console.error("oppus failed to excute");
+
+  
+
+
