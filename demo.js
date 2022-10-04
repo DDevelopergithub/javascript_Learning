36961e98b168501544e62a0d200ec328cbaaa2f0
DDevelopergithub <tejaswinibhosale787@gmail.com>
2022-10-04 17:42:03 +0530

    file execution in js
    file execution in js

--- demo.js
+++ demo.js
@@ -1,12 +1,15 @@
    //This is my first program
-  console.log("Hello");
-  console.log("My name is Mohit sharma");
+  console.log("My name is:-");
+           console.log("Tejaswini Nagesh Bhosale");
   //info means information
-  console.info("This is info");
+  console.info("My Hobbies:-");
+    console.info("Drawing,Lisning the songs");
   //warn means warning
-  console.warn("I am not that bad guy");
-  console.error("oppus failed to excute");
+  console.warn("this is studio");
+  console.error("error");
+  console.log("25");
+  console.log("125");
 
-  
+
 
 
