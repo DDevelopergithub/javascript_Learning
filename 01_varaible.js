d8c0b9f6eaa2e757724b97bbee835cbc5fa171ce
DDevelopergithub <tejaswinibhosale787@gmail.com>
2022-10-06 08:52:33 +0530

    add the files
    add the files

---
+++ 01_varaible.js
@@ -0,0 +1,10 @@
+var my_apple; //var is decleration 
+my_apple="Apple"; //varaiable initization
+
+//var my_name ="Mohit Sharma";//var is variable and inisilization on the same line
+//my_name="Mohit Raghva Sharma ";
+//console.log(my_name);
+console.log(my_apple);
+
+my_apple= "200";
+console.log(my_apple);
